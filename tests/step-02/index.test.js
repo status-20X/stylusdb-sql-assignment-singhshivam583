2b947b7308860b27adc6880c8e9c5ea5cb0afa72
Shvam Singh <singhshivam583@yahoo.in>
2024-04-15 17:43:47 +0530

    step-8

--- tests/step-02/index.test.js
+++ tests/step-02/index.test.js
@@ -1,7 +1,9 @@
 const readCSV = require('../../src/csvReader');
+const parseQuery = require('../../src/queryParser');
+const executeSELECTQuery = require('../../src/index');
 
 test('Read CSV File', async () => {
-    const data = await readCSV('./sample.csv');
+    const data = await readCSV('./student.csv');
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
     expect(data[0].name).toBe('John');
